1eb45b6a02ad45ce3c9597c5ec9ba22db3136e40
HOCKERSGAMING <87328738+HOCKERSGAMING@users.noreply.github.com>
2023-07-14 13:09:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -32,7 +32,7 @@ async def start(client, message):
     )
 
 
-@trojanz.on_message(filters.command(["help"]) & filters.private)
+@Hg_Anime.on_message(filters.command(["help"]) & filters.private)
 async def help(client, message):
     await message.reply_text(
         text=Script.HELP_MSG,
@@ -53,7 +53,7 @@ async def help(client, message):
     )
 
 
-@trojanz.on_message(filters.command(["about"]) & filters.private)
+@Hg_Anime.on_message(filters.command(["about"]) & filters.private)
 async def about(client, message):
     await message.reply_text(
         text=Script.ABOUT_MSG,
